073c97868cae09aae570c139b8c5fb3be2036b3e
Thanh-Quang12 <ntquang1212@gmail.com>
2025-06-22 20:58:45 +0700

    Update fKhachHang.cs

--- LKMT/GUI/fKhachHang.cs
+++ LKMT/GUI/fKhachHang.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BUS;
+
 namespace LKMT.GUI
 {
     public partial class fKhachHang : UserControl
@@ -16,14 +17,14 @@ namespace LKMT.GUI
         {
             InitializeComponent();
             KhachHangBUS.Instance.showKhachHang(dgvKhachHang);
-            dgvKhachHang.Columns[0].HeaderText = "Mã khách hàng";
-            dgvKhachHang.Columns[1].HeaderText = "Tên khách hàng";
+            dgvKhachHang.Columns[0].HeaderText = "Ma khach hang";
+            dgvKhachHang.Columns[1].HeaderText = "Ten khach hang";
             dgvKhachHang.Columns[2].HeaderText = "Email";
-            dgvKhachHang.Columns[3].HeaderText = "Mật khẩu";
-            dgvKhachHang.Columns[4].HeaderText = "Số điện thoại";
-            dgvKhachHang.Columns[5].HeaderText = "Địa chỉ";
-            dgvKhachHang.Columns[6].HeaderText = "Ngày tạo";
-            dgvKhachHang.Columns[7].HeaderText = "Ngày cập nhật";
+            dgvKhachHang.Columns[3].HeaderText = "Mat khau";
+            dgvKhachHang.Columns[4].HeaderText = "So dien thoai";
+            dgvKhachHang.Columns[5].HeaderText = "Dia chi";
+            dgvKhachHang.Columns[6].HeaderText = "Ngay tao";
+            dgvKhachHang.Columns[7].HeaderText = "Ngay cap nhat";
 
             dgvKhachHang.Columns[0].Width = 50;
             dgvKhachHang.Columns[1].Width = 170;
@@ -42,19 +43,20 @@ namespace LKMT.GUI
 
         private void btnThem_Click(object sender, EventArgs e)
         {
-           if (txtTenKH.TextLength == 0)
-                MessageBox.Show("Tên không được bỏ trống!!", "Thông Báo", MessageBoxButtons.OK);
-           else if (txtDienThoai.TextLength == 0)
-                MessageBox.Show("Số điện thoại không được bỏ trống!!", "Thông Báo", MessageBoxButtons.OK);
+            if (txtTenKH.TextLength == 0)
+                MessageBox.Show("Ten khong duoc bo trong!!", "Thong Bao", MessageBoxButtons.OK);
+            else if (txtDienThoai.TextLength == 0)
+                MessageBox.Show("So dien thoai khong duoc bo trong!!", "Thong Bao", MessageBoxButtons.OK);
             else
             {
-                if (KhachHangBUS.Instance.themKhachHang(txtTenKH.Text,txtEmail.Text,txtMatKhau.Text,txtDienThoai.Text,txtDiaChi.Text))
+                if (KhachHangBUS.Instance.themKhachHang(txtTenKH.Text, txtEmail.Text, txtMatKhau.Text, txtDienThoai.Text, txtDiaChi.Text))
                 {
-                    MessageBox.Show("Thêm khách hàng thành công!!", "Thông Báo", MessageBoxButtons.OK);
+                    MessageBox.Show("Them khach hang thanh cong!!", "Thong Bao", MessageBoxButtons.OK);
                     KhachHangBUS.Instance.showKhachHang(dgvKhachHang);
                     btnLamMoi_Click(sender, e);
                 }
-                else MessageBox.Show("Thêm khách hàng thất bại!!", "Thông Báo", MessageBoxButtons.OK);
+                else
+                    MessageBox.Show("Them khach hang that bai!!", "Thong Bao", MessageBoxButtons.OK);
             }
         }
 
@@ -91,32 +93,33 @@ namespace LKMT.GUI
             txtSearchBox.Text = null;
             KhachHangBUS.Instance.showKhachHang(dgvKhachHang);
         }
+
         private void btnSua_Click(object sender, EventArgs e)
         {
             Int32 selectedRowCount = dgvKhachHang.Rows.GetRowCount(DataGridViewElementStates.Selected);
             if (selectedRowCount > 0)
             {
-                if (KhachHangBUS.Instance.suaKhachHang(int.Parse(txtMaKH.Text),txtTenKH.Text, txtEmail.Text, txtMatKhau.Text, txtDienThoai.Text, txtDiaChi.Text, DateTime.Parse(txtNgayTao.Text)))
+                if (KhachHangBUS.Instance.suaKhachHang(int.Parse(txtMaKH.Text), txtTenKH.Text, txtEmail.Text, txtMatKhau.Text, txtDienThoai.Text, txtDiaChi.Text, DateTime.Parse(txtNgayTao.Text)))
                 {
-                    MessageBox.Show("Cập nhật khách hàng thành công!!", "Thông Báo", MessageBoxButtons.OK);
+                    MessageBox.Show("Cap nhat khach hang thanh cong!!", "Thong Bao", MessageBoxButtons.OK);
                     KhachHangBUS.Instance.showKhachHang(dgvKhachHang);
                     btnLamMoi_Click(sender, e);
                 }
-                else MessageBox.Show("Cập nhật khách hàng thất bại!!", "Thông Báo", MessageBoxButtons.OK);
+                else
+                    MessageBox.Show("Cap nhat khach hang that bai!!", "Thong Bao", MessageBoxButtons.OK);
             }
             else
             {
-                MessageBox.Show("Vui lòng chọn khách hàng muốn cập nhật!!", "Thông Báo", MessageBoxButtons.OK);
+                MessageBox.Show("Vui long chon khach hang muon cap nhat!!", "Thong Bao", MessageBoxButtons.OK);
             }
         }
 
         private void btnTimKiem_Click(object sender, EventArgs e)
         {
             if (txtSearchBox.TextLength == 0)
-                MessageBox.Show("Vui lòng nhập số điện thoại!!", "Thông Báo", MessageBoxButtons.OK);
-            if(KhachHangBUS.Instance.timKhachHangbySDT(dgvKhachHang,txtSearchBox.Text))        
-                 MessageBox.Show("Không tìm thấy khách hàng có số điện thoại " + txtSearchBox.Text, "Thông Báo", MessageBoxButtons.OK);
-
+                MessageBox.Show("Vui long nhap so dien thoai!!", "Thong Bao", MessageBoxButtons.OK);
+            if (KhachHangBUS.Instance.timKhachHangbySDT(dgvKhachHang, txtSearchBox.Text))
+                MessageBox.Show("Khong tim thay khach hang co so dien thoai " + txtSearchBox.Text, "Thong Bao", MessageBoxButtons.OK);
         }
     }
 }
