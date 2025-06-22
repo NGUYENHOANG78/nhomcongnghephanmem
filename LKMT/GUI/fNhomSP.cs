55bb37ab975cc075bc86f13fcf1d1933d6d595f4
Thanh-Quang12 <ntquang1212@gmail.com>
2025-06-22 20:58:42 +0700

    Update fNhomSP.cs

--- LKMT/GUI/fNhomSP.cs
+++ LKMT/GUI/fNhomSP.cs
@@ -16,10 +16,10 @@ namespace LKMT.GUI
         {
             InitializeComponent();
             NhomSanPhamBUS.Instance.showNhomSP(dgvNhomSP);
-            dgvNhomSP.Columns[0].HeaderText = "Mã nhóm";
-            dgvNhomSP.Columns[1].HeaderText = "Tên nhóm linh kiện";
-            dgvNhomSP.Columns[2].HeaderText = "Ngày tạo";
-            dgvNhomSP.Columns[3].HeaderText = "Ngày cập nhật";
+            dgvNhomSP.Columns[0].HeaderText = "Ma nhom";
+            dgvNhomSP.Columns[1].HeaderText = "Ten nhom linh kien";
+            dgvNhomSP.Columns[2].HeaderText = "Ngay tao";
+            dgvNhomSP.Columns[3].HeaderText = "Ngay cap nhat";
             dgvNhomSP.Columns[0].Width = 70;
             dgvNhomSP.Columns[1].Width = 250;
             dgvNhomSP.Columns[2].Width = 100;
@@ -41,21 +41,21 @@ namespace LKMT.GUI
         {
            if(txtID.TextLength>5)
            {
-                MessageBox.Show("Mã không được vượt quá 5 ký tự!!", "Thông Báo", MessageBoxButtons.OK);
+                MessageBox.Show("Ma khong duoc vuot qua 5 ky tu!!", "Thong Bao", MessageBoxButtons.OK);
            }
            else if (txtID.TextLength == 0)
-                MessageBox.Show("Mã không được bỏ trống!!", "Thông Báo", MessageBoxButtons.OK);
+                MessageBox.Show("Ma khong duoc bo trong!!", "Thong Bao", MessageBoxButtons.OK);
            else if(txtName.TextLength==0)
-                MessageBox.Show("Tên không được bỏ trống!!", "Thông Báo", MessageBoxButtons.OK);
+                MessageBox.Show("Ten khong duoc bo trong!!", "Thong Bao", MessageBoxButtons.OK);
            else
             {
                 if (NhomSanPhamBUS.Instance.themNhomSp(txtID.Text, txtName.Text))
                 {
-                    MessageBox.Show("Thêm nhóm sản phẩm thành công!!", "Thông Báo", MessageBoxButtons.OK);
+                    MessageBox.Show("Them nhom san pham thanh cong!!", "Thong Bao", MessageBoxButtons.OK);
                     NhomSanPhamBUS.Instance.showNhomSP(dgvNhomSP);
                     btnLamMoi_Click(sender, e);
                 }
-                else MessageBox.Show("Thêm nhóm sản phẩm thất bại!!", "Thông Báo", MessageBoxButtons.OK);
+                else MessageBox.Show("Them nhom san pham that bai!!", "Thong Bao", MessageBoxButtons.OK);
             }          
         }
 
@@ -66,16 +66,16 @@ namespace LKMT.GUI
             {
                 if (NhomSanPhamBUS.Instance.xoaNhomSP(txtID.Text))
                 {
-                    MessageBox.Show("Xóa nhóm linh kiện thành công!!", "Thông Báo", MessageBoxButtons.OK);
+                    MessageBox.Show("Xoa nhom linh kien thanh cong!!", "Thong Bao", MessageBoxButtons.OK);
                     NhomSanPhamBUS.Instance.showNhomSP(dgvNhomSP);
                     btnLamMoi_Click(sender, e);
                 }
-                else MessageBox.Show("Không thể xóa nhóm linh kiện này, vì còn tồn tại loại linh kiện hoặc thương hiệu thuộc nhóm này!!", "Thông Báo", MessageBoxButtons.OK);
+                else MessageBox.Show("Khong the xoa nhom linh kien nay, vi con ton tai loai linh kien hoac thuong hieu thuoc nhom nay!!", "Thong Bao", MessageBoxButtons.OK);
                 btnLamMoi_Click(sender, e);
             }
             else
             {
-                MessageBox.Show("Vui lòng chọn loại linh kiện muốn xóa!!", "Thông Báo", MessageBoxButtons.OK);
+                MessageBox.Show("Vui long chon loai linh kien muon xoa!!", "Thong Bao", MessageBoxButtons.OK);
             }
         }
 
@@ -86,15 +86,15 @@ namespace LKMT.GUI
             {
                 if (NhomSanPhamBUS.Instance.suaNhomSP(txtID.Text, txtName.Text, DateTime.Parse(txtNgayTao.Text)))
                 {
-                    MessageBox.Show("Cập nhật nhóm linh kiện thành công!!", "Thông Báo", MessageBoxButtons.OK);
+                    MessageBox.Show("Cap nhat nhom linh kien thanh cong!!", "Thong Bao", MessageBoxButtons.OK);
                     NhomSanPhamBUS.Instance.showNhomSP(dgvNhomSP);
                     btnLamMoi_Click(sender, e);
                 }
-                else MessageBox.Show("Cập nhật nhóm linh kiện thất bại!!", "Thông Báo", MessageBoxButtons.OK);
+                else MessageBox.Show("Cap nhat nhom linh kien that bai!!", "Thong Bao", MessageBoxButtons.OK);
             }
             else
             {
-                MessageBox.Show("Vui lòng chọn nhóm linh kiện muốn cập nhật!!", "Thông Báo", MessageBoxButtons.OK);
+                MessageBox.Show("Vui long chon nhom linh kien muon cap nhat!!", "Thong Bao", MessageBoxButtons.OK);
             }         
         }
         private void dgvNhomSP_CellClick(object sender, DataGridViewCellEventArgs e)
