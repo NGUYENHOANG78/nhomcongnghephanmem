9fab671b473e440eee8defdba0840f9a4a662335
Qthang <tranhuynhquocthang12@gmail.com>
2025-07-03 09:14:55 +0700

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,7 +1,17 @@
+<<<<<<< HEAD
 ﻿insert into admin (email,matkhau,ten,sodienthoai) values ('thuannef','123','Phạm Trần Thanh Thuận','0965349315')
 insert into admin (email,matkhau,ten,sodienthoai) values ('thien','123','Phạm Trần Thanh Thuận','0965349315')
 insert into admin (email,matkhau,ten,sodienthoai) values ('hoang','123','Phạm Lê Nguyễn Hoàng','0356783513')
 insert into admin (email,matkhau,ten,sodienthoai) values ('hoang','1234','Phạm Lê Nguyễn Hoàng','0356783513')
+insert into admin (email,matkhau,ten,sodienthoai) values ('hung','1234','Nguyễn Thịnh Hưng','0356783513')
+insert into admin (email,matkhau,ten,sodienthoai) values ('thang','1234','Trần Huỳnh Quốc Thắng','0356783513')
+=======
+﻿insert into admin (email,matkhau,ten,sodienthoai) values ('quangne','123','nguyễn thanh quang','0965349315')
+insert into admin (email,matkhau,ten,sodienthoai) values ('hoang','123','Phạm lê nguyễn hoàng','0965349315')
+insert into admin (email,matkhau,ten,sodienthoai) values ('hung','123','nguyễn thinh hưng','0356783513')
+insert into admin (email,matkhau,ten,sodienthoai) values ('thang','1234','trần huỳnh quốc thắng','0356783513')
+insert into admin (email,matkhau,ten,sodienthoai) values ('hoang','1234','Võ trần hoàng','0356783513')
+>>>>>>> ec939c7dab81e95fa29b3833ae26b3c50d9538de
 
 insert into nhomsanpham values ('RAM','Ram - Bộ nhớ trong')
 insert into nhomsanpham values ('CPU','Cpu - Bộ vi xử lí')
