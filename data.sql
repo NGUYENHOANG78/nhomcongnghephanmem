1de85d0aaaf9a200fd2b39ba87237d45f5f5ae7d
NGUYENHOANG78 <phamlenguyenhoang19@gmail.com>
2025-07-03 12:47:50 +0700

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+﻿<<<<<<< HEAD
 ﻿insert into admin (email,matkhau,ten,sodienthoai) values ('thuannef','123','Phạm Trần Thanh Thuận','0965349315')
 insert into admin (email,matkhau,ten,sodienthoai) values ('thien','123','Phạm Trần Thanh Thuận','0965349315')
 insert into admin (email,matkhau,ten,sodienthoai) values ('hoang','123','Phạm Lê Nguyễn Hoàng','0356783513')
@@ -13,7 +13,7 @@ insert into admin (email,matkhau,ten,sodienthoai) values ('thang','1234','trần
 insert into admin (email,matkhau,ten,sodienthoai) values ('hoang','1234','Võ trần hoàng','0356783513')
 >>>>>>> ec939c7dab81e95fa29b3833ae26b3c50d9538de
 
-insert into nhomsanpham values ('RAM','Ram - Bộ nhớ trong')
+insert into nhomsanpham values ('R','Ram - Bộ nhớ trong')
 insert into nhomsanpham values ('CPU','Cpu - Bộ vi xử lí')
 insert into nhomsanpham values ('MAIN','Mainboard - Bo mạch chủ')
 insert into nhomsanpham values ('SSD','SSD - Ổ cứng thể rắn')
